6bafb0176f7199639ebd29226bcecff8c0e5d395
Jorge Luna <jorge.luna@salesforce.com>
2022-09-07 13:34:49 -0400

    sync-deploy

--- app.js
+++ app.js
@@ -68,34 +68,11 @@ async function StartServer() {
     });
   });
 
-  // Listens to incoming messages that contain "hello"
-  app.message("hello", async ({ message, say }) => {
+  // Just a quick verification than the bot is alive.
+  app.message("are you there?", async ({ message, say }) => {
     await say({
-      blocks: [
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `Hey there <@${message.user}> ===>  `
-          },
-          accessory: {
-            type: "button",
-            text: {
-              type: "plain_text",
-              text: "Click Me"
-            },
-            action_id: "button_click"
-          }
-        }
-      ],
-      text: `Hey there <@${message.user}>!`
+      text: `Yes <@${message.user}>! I am here.`
     });
-  });
-
-  app.action("button_click", async ({ body, ack, say }) => {
-    // Acknowledge the action
-    await ack();
-    await say(`<@${body.user.id}> clicked the button`);
   });
 
   await app.start(process.env.PORT || 3000);
