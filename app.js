ef16d00b571f085ea1da30263ce3adb11089b117
Jorge Luna <jorge.luna@salesforce.com>
2022-09-08 00:02:47 -0400

    sync-deploy

--- app.js
+++ app.js
@@ -142,26 +142,34 @@ async function StartServer() {
 
   app.action("button_yes_click", async ({ body, ack, say }) => {
     // Acknowledge the action
-    logger.info("'Yes' button clicked", body);
-    // const payload = JSON.parse(body.actions.value);
-    // const number = payload.number;
-    // const appointmentId = payload.appointmentId;
-    // const threadId = payload.threadId;
-    await say("ok!");
+    logger.info("'No' button clicked", body.message.actions[0].value);
+    const payload = JSON.parse(body.message.actions[0].value);
+    const number = payload.number;
+    const appointmentId = payload.appointmentId;
+    const threadId = payload.threadId;
+    await say({
+      text: "ok!",
+      channel: FS_ASSISTANT_CHANNEL,
+      thread_ts: threadId
+    });
     await ack();
-    // onAcceptAppointment({ number, appointmentId, threadId });
+    onAcceptAppointment({ number, appointmentId, threadId });
   });
 
   app.action("button_no_click", async ({ body, ack, say }) => {
     // Acknowledge the action
-    logger.info("'No' button clicked", body);
-    // const payload = JSON.parse(body.actions.value);
-    // const number = payload.number;
-    // const appointmentId = payload.appointmentId;
-    // const threadId = payload.threadId;
-    await say("ok!");
+    logger.info("'No' button clicked", body.message.actions[0].value);
+    const payload = JSON.parse(body.message.actions[0].value);
+    const number = payload.number;
+    const appointmentId = payload.appointmentId;
+    const threadId = payload.threadId;
+    await say({
+      text: "ok!",
+      channel: FS_ASSISTANT_CHANNEL,
+      thread_ts: threadId
+    });
     await ack();
-    // onDeclineAppointment({ number, appointmentId, threadId });
+    onDeclineAppointment({ number, appointmentId, threadId });
   });
 
   const onAcceptAppointment = async ({ number, appointmentId, threadId }) => {
