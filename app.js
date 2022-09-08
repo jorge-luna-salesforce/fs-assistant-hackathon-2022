77cacda25181ce35532bfcd04e646213bdf4c576
Jorge Luna <jorge.luna@salesforce.com>
2022-09-08 08:33:49 -0400

    sync-deploy

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { App, ExpressReceiver } = require("@slack/bolt");
 const bodyParser = require("body-parser");
 const logBunyan = require("@google-cloud/logging-bunyan");
+const fetch = require("node-fetch");
 const { slackMessageCreator, slackAcceptanceReplyCreator, smsMessageCreator, tinifyURL } = require("./util/templateFunctions");
 
 const FS_ASSISTANT_CHANNEL = "C040SH1GX5Z";
